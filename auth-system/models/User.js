f70e90b89f57da9fa49710dd636a0d4458130451
Austin <austin752@gmail.com>
2018-11-02 21:18:07 -0400

    commit

--- auth-system/models/User.js
+++ auth-system/models/User.js
@@ -1,43 +1,62 @@
-var Sequelize = require('sequelize');
+var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
-// create a sequelize instance with our local postgres database information.
-var sequelize = new Sequelize('postgres://postgres@localhost:5432/auth-system');
-
-// setup User model and its fields.
-var User = sequelize.define('users', {
-    username: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-}, {
-    hooks: {
-      beforeCreate: (user) => {
-        const salt = bcrypt.genSaltSync();
-        user.password = bcrypt.hashSync(user.password, salt);
-      }
-    },
-    instanceMethods: {
-      validPassword: function(password) {
-        return bcrypt.compareSync(password, this.password);
-      }
-    }    
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  passwordConf: {
+    type: String,
+    required: true,
+  }
 });
 
-// create all the defined tables in the specified database.
-sequelize.sync()
-    .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
-    .catch(error => console.log('This error occured', error));
+//authenticate input against database
+UserSchema.statics.authenticate = function (email, password, callback) {
+  User.findOne({ email: email })
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    });
+}
 
-// export User model for use in other files.
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+
+var User = mongoose.model('User', UserSchema);
 module.exports = User;
--- server.js
+++ server.js
@@ -3,3 +3,61 @@ var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
+
+// const express = require('express');
+// const exp = express();
+// const jwt = require('express-jwt');
+// const jwtAuthz = require('express-jwt-authz');
+// const jwksRsa = require('jwks-rsa');
+
+// // Authentication middleware. When used, the
+// // Access Token must exist and be verified against
+// // the Auth0 JSON Web Key Set
+// const checkJwt = jwt({
+//   // Dynamically provide a signing key
+//   // based on the kid in the header and 
+//   // the signing keys provided by the JWKS endpoint.
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: `https://YOUR_AUTH0_DOMAIN/.well-known/jwks.json`
+//   }),
+
+//   // Validate the audience and the issuer.
+//   audience: 'YOUR_API_IDENTIFIER',
+//   issuer: `https://YOUR_AUTH0_DOMAIN/`,
+//   algorithms: ['RS256']
+// });
+
+// // This route doesn't need authentication
+// exp.get('/api/public', function(req, res) {
+//   res.json({
+//     message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+//   });
+// });
+
+// // This route need authentication
+// exp.get('/api/private', checkJwt, function(req, res) {
+//   res.json({
+//     message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+//   });
+// });
+
+// const checkScopes = jwtAuthz([ 'read:messages' ]);
+
+// exp.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+//   res.json({
+//     message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+//   });
+// });
+
+// //use sessions for tracking logins
+// exp.use(session({
+//   secret: 'work hard',
+//   resave: true,
+//   saveUninitialized: false,
+//   store: new MongoStore({
+//     mongooseConnection: db
+//   })
+// }));
